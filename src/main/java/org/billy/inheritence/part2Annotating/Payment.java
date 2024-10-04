0050065642c803d59e68862b924d11d10f6be1fd
wilbill <ntabazibilly57@gmail.com>
2024-10-04 08:03:06 -0500

    last commits

--- src/main/java/org/billy/inheritence/part2Annotating/Payment.java
+++ src/main/java/org/billy/inheritence/part2Annotating/Payment.java
@@ -1,5 +1,6 @@
 package org.billy.inheritence.part2Annotating;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -9,7 +10,8 @@ import java.util.Date;
 @Embeddable
 public class Payment {
     @Temporal(TemporalType.DATE)
+    @Column(name="PAYDATE")
     private Date payDate;
-    private String paydate;
+    @Column(name="AMOUNT")
     private double amount;
 }
